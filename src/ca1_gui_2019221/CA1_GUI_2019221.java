38b8de4c28a7675cd0f44cce4989b786f65dd9b7
Leo Macieski <leojkmacieski@hotmail.com>
2020-11-06 10:36:31 +0000

    Initial commit

---
+++ src/ca1_gui_2019221/CA1_GUI_2019221.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca1_gui_2019221;
+
+/**
+ *
+ * @author leojk
+ */
+public class CA1_GUI_2019221 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
